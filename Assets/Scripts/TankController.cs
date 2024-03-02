95782ac91b7b7eb304266de2fa11b12fc281bc75
Nohbdy <NicolasCarty03@gmail.com>
2024-03-01 19:13:05 -0500

    Revert TankController to MonoBehaviour

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,22 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NETWORK_ENGINE;
 
-public class TankController : NetworkComponent
+public class TankController : MonoBehaviour
 {
-    public override void HandleMessage(string flag, string value)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void NetworkedStart()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override IEnumerator SlowUpdate()
-    {
-        throw new System.NotImplementedException();
-    }
+    
 }
