195d2abc66dd40078af33a2d0b6afac8f6aa856d
Nohbdy <NicolasCarty03@gmail.com>
2024-03-01 19:12:15 -0500

    Add TankController, remove canvas

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+
+public class TankController : NetworkComponent
+{
+    public override void HandleMessage(string flag, string value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void NetworkedStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
